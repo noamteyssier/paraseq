112baac4280a685a39b7581997c4315da4b72599
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-01-29 13:21:44 -0800

    feat: added examples for reloading in parallel and a naive reader

---
+++ examples/naive.rs
@@ -0,0 +1,54 @@
+use std::{fs::File, io::Read};
+
+use anyhow::Result;
+use paraseq::fastq;
+
+fn naive_fastq<R: Read>(rdr: R) -> Result<()> {
+    let (pass, _comp) = niffler::get_reader(Box::new(rdr))?;
+
+    let mut reader = fastq::Reader::new(pass);
+    let mut rset = fastq::RecordSet::default();
+
+    let mut num_records = 0;
+    while rset.fill(&mut reader)? {
+        for record in rset.iter() {
+            let _record = record?;
+            num_records += 1;
+        }
+    }
+    eprintln!("Number of records: {}", num_records);
+
+    Ok(())
+}
+
+fn naive_fasta<R: Read>(rdr: R) -> Result<()> {
+    let (pass, _comp) = niffler::get_reader(Box::new(rdr))?;
+
+    let mut reader = paraseq::fasta::Reader::new(pass);
+    let mut rset = paraseq::fasta::RecordSet::default();
+
+    let mut num_records = 0;
+    while rset.fill(&mut reader)? {
+        for record in rset.iter() {
+            let _record = record?;
+            num_records += 1;
+        }
+    }
+    eprintln!("Number of records: {}", num_records);
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let path = std::env::args().nth(1).expect("Missing input file");
+    let file = File::open(&path)?;
+    if path.ends_with(".fastq") {
+        naive_fastq(file)?;
+    } else if path.ends_with(".fasta") {
+        naive_fasta(file)?;
+    } else {
+        eprintln!("Unknown file format");
+    }
+
+    Ok(())
+}
---
+++ examples/reloading_parallel.rs
@@ -0,0 +1,82 @@
+use std::fs::File;
+use std::sync::Arc;
+
+use anyhow::{bail, Result};
+use paraseq::{
+    fastq,
+    parallel::{ParallelProcessor, ParallelReader},
+};
+use parking_lot::Mutex;
+
+#[derive(Default, Clone)]
+pub struct SeqSum {
+    // thread local sum of nucleotides
+    local_sum: u64,
+
+    // thread local number of records
+    local_num: u64,
+
+    // global sum of nucleotides
+    sum: Arc<Mutex<u64>>,
+
+    // global number of records
+    num: Arc<Mutex<u64>>,
+}
+impl SeqSum {
+    pub fn get_sum(&self) -> u64 {
+        *self.sum.lock()
+    }
+    pub fn get_num(&self) -> u64 {
+        *self.num.lock()
+    }
+}
+impl ParallelProcessor for SeqSum {
+    fn process_record<Rf: paraseq::fastx::Record>(
+        &mut self,
+        record: Rf,
+    ) -> paraseq::parallel::Result<()> {
+        record
+            .seq()
+            .iter()
+            .for_each(|b| self.local_sum += *b as u64);
+        self.local_num += 1;
+        Ok(())
+    }
+    fn on_batch_complete(&mut self) -> paraseq::parallel::Result<()> {
+        *self.sum.lock() += self.local_sum;
+        *self.num.lock() += self.local_num;
+        self.local_sum = 0;
+        self.local_num = 0;
+        Ok(())
+    }
+}
+
+fn main() -> Result<()> {
+    let path = std::env::args().nth(1).unwrap();
+    let file = File::open(&path)?;
+
+    let mut reader = fastq::Reader::new(file);
+    let mut rset = fastq::RecordSet::new(1);
+
+    if !rset.fill(&mut reader)? {
+        bail!("No sequences in input")
+    };
+    let mut num_prefill = 0;
+    for record in rset.iter() {
+        record?;
+        num_prefill += 1;
+    }
+    eprintln!("read {} records in prefill", num_prefill);
+
+    // Reload the reader
+    reader.reload(&mut rset);
+
+    // Parallel process the reader
+    let proc = SeqSum::default();
+    reader.process_parallel(proc.clone(), 4)?;
+
+    eprintln!("num_records: {}", proc.get_num());
+    eprintln!("sum: {}", proc.get_sum());
+
+    Ok(())
+}
