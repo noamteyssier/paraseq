ee9be4d056c08b0f96d1679e60bcc991e78a930e
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-01-28 16:51:02 -0800

    feat: added example of parallel processor

---
+++ examples/paired_parallel.rs
@@ -0,0 +1,95 @@
+use std::fs::File;
+use std::sync::Arc;
+
+// use anyhow::Result;
+use paraseq::{
+    fasta, fastq,
+    fastx::Record,
+    parallel::{PairedParallelProcessor, PairedParallelReader, ProcessError},
+};
+use parking_lot::Mutex;
+
+#[derive(Default, Clone)]
+pub struct SeqSum {
+    /// Thread local sum of bytes in the sequence
+    pub byte_sum: u64,
+    /// Thread local number of records
+    pub num_records: u64,
+
+    /// Global sum of bytes in the sequence
+    pub global_byte_sum: Arc<Mutex<u64>>,
+    /// Global number of records
+    pub global_num_records: Arc<Mutex<u64>>,
+}
+impl SeqSum {
+    pub fn get_num_records(&self) -> u64 {
+        *self.global_num_records.lock()
+    }
+    pub fn get_byte_sum(&self) -> u64 {
+        *self.global_byte_sum.lock()
+    }
+}
+impl PairedParallelProcessor for SeqSum {
+    fn process_record_pair<Rf: Record>(
+        &mut self,
+        record1: Rf,
+        record2: Rf,
+    ) -> Result<(), ProcessError> {
+        for _ in 0..100 {
+            // Simulate some work
+            record1
+                .seq()
+                .iter()
+                .for_each(|b| self.byte_sum += *b as u64);
+            record2
+                .seq()
+                .iter()
+                .for_each(|b| self.byte_sum += *b as u64);
+        }
+        self.num_records += 1;
+        Ok(())
+    }
+    fn on_batch_complete(&mut self) -> Result<(), ProcessError> {
+        *self.global_byte_sum.lock() += self.byte_sum;
+        *self.global_num_records.lock() += self.num_records;
+        self.byte_sum = 0;
+        self.num_records = 0;
+        Ok(())
+    }
+}
+
+fn main() -> Result<(), ProcessError> {
+    let path_r1 = std::env::args()
+        .nth(1)
+        .unwrap_or("./data/sample_R1.fastq".to_string());
+    let path_r2 = std::env::args()
+        .nth(2)
+        .unwrap_or("./data/sample_R2.fastq".to_string());
+
+    let num_threads = std::env::args()
+        .nth(3)
+        .unwrap_or("1".to_string())
+        .parse::<usize>()
+        .unwrap_or(1);
+
+    let file_r1 = File::open(&path_r1)?;
+    let file_r2 = File::open(&path_r2)?;
+    let processor = SeqSum::default();
+
+    if path_r1.ends_with(".fastq") {
+        let rdr_r1 = fastq::Reader::new(file_r1);
+        let rdr_r2 = fastq::Reader::new(file_r2);
+        rdr_r1.process_parallel_paired(rdr_r2, processor.clone(), num_threads)?;
+    } else if path_r1.ends_with(".fasta") {
+        let rdr_r1 = fasta::Reader::new(file_r1);
+        let rdr_r2 = fasta::Reader::new(file_r2);
+        rdr_r1.process_parallel_paired(rdr_r2, processor.clone(), num_threads)?;
+    } else {
+        panic!("Unknown file format");
+    }
+
+    println!("num_records: {}", processor.get_num_records());
+    println!("byte_sum: {}", processor.get_byte_sum());
+
+    Ok(())
+}
