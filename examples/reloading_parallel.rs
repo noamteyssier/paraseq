4d3e50f91c2fefc6162b6e251b855ace518a5147
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-06-06 16:47:27 -0700

    chore: added example for fasta and fastq for reloading in parallel

--- examples/reloading_parallel.rs
+++ examples/reloading_parallel.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 
 use anyhow::{bail, Result};
 use paraseq::{
-    fastq,
+    fasta, fastq,
     parallel::{ParallelProcessor, ParallelReader},
 };
 use parking_lot::Mutex;
@@ -53,10 +53,8 @@ impl ParallelProcessor for SeqSum {
     }
 }
 
-fn main() -> Result<()> {
-    let path = std::env::args().nth(1).unwrap();
+fn reload_fastq(path: &str, n_threads: usize) -> Result<()> {
     let file = File::open(&path)?;
-
     let mut reader = fastq::Reader::new(file);
     let mut rset = fastq::RecordSet::new(1);
 
@@ -75,10 +73,48 @@ fn main() -> Result<()> {
 
     // Parallel process the reader
     let proc = SeqSum::default();
-    reader.process_parallel(proc.clone(), 4)?;
+    reader.process_parallel(proc.clone(), n_threads)?;
 
     eprintln!("num_records: {}", proc.get_num());
     eprintln!("sum: {}", proc.get_sum());
 
     Ok(())
+}
+
+fn reload_fasta(path: &str, n_threads: usize) -> Result<()> {
+    let file = File::open(&path)?;
+    let mut reader = fasta::Reader::new(file);
+    let mut rset = fasta::RecordSet::new(1);
+
+    if !rset.fill(&mut reader)? {
+        bail!("No sequences in input")
+    }
+    let mut num_prefill = 0;
+    for record in rset.iter() {
+        record?;
+        num_prefill += 1;
+    }
+    eprintln!("read {num_prefill} records in prefill");
+
+    // Reload the reader
+    reader.reload(&mut rset);
+
+    // Parallel process the reader
+    let proc = SeqSum::default();
+    reader.process_parallel(proc.clone(), n_threads)?;
+
+    eprintln!("num_records: {}", proc.get_num());
+    eprintln!("sum: {}", proc.get_sum());
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let path = std::env::args().nth(1).unwrap();
+    let threads = std::env::args().nth(2).unwrap().parse::<usize>()?;
+    if path.ends_with(".fasta") {
+        reload_fasta(&path, threads)
+    } else {
+        reload_fastq(&path, threads)
+    }
 }
