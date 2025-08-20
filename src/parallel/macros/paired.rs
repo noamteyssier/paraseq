813828905395cc9403856de55b707519827b3e3e
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2025-08-20 15:48:17 +0200

    replace paired-end macro by trait

--- src/parallel/macros/paired.rs
+++ src/parallel/macros/paired.rs
@@ -5,6 +5,8 @@ use parking_lot::Mutex;
 
 use crate::parallel::error::{ProcessError, RecordPair, Result};
 use crate::parallel::{PairedParallelProcessor, PairedParallelReader};
+use crate::prelude::ParallelReader;
+use crate::Record;
 
 /// Type alias for synchronized record sets containing pairs of records
 type PairedRecordSets<T> = Arc<Vec<Mutex<(T, T)>>>;
@@ -109,169 +111,220 @@ where
     Ok(())
 }
 
-/// Macro to implement parallel reader for paired reads
-macro_rules! impl_paired_parallel_reader {
-    ($reader:ty, $record_set:ty, $error:ty) => {
-        impl<R> PairedParallelReader<R> for $reader
-        where
-            R: io::Read + Send,
-        {
-            fn process_parallel_paired<T>(
-                self,
-                reader2: Self,
-                processor: T,
-                num_threads: usize,
-            ) -> Result<()>
-            where
-                T: PairedParallelProcessor,
-            {
-                if num_threads == 0 {
-                    return Err(ProcessError::InvalidThreadCount);
-                }
-                if num_threads == 1 {
-                    return self.process_sequential_paired(reader2, processor);
-                }
-                let record_sets = Arc::new(
-                    (0..num_threads * 2)
-                        .map(|_| Mutex::new((self.new_record_set(), reader2.new_record_set())))
-                        .collect::<Vec<_>>(),
-                );
-                let (tx, rx) = create_channels(num_threads * 2);
-                let (shutdown_tx, shutdown_rx) = create_shutdown_channel();
+trait PairedParallelReaderSupport: Send {
+    type RecordSet: Default + Send;
+    type Error;
+    type RefRecord<'a>: Record;
 
-                thread::scope(|scope| -> Result<()> {
-                    // Spawn reader thread
-                    let reader_sets = Arc::clone(&record_sets);
-                    let reader_handle = scope.spawn(move || -> Result<()> {
-                        run_paired_reader_thread(
-                            self,
-                            reader2,
-                            reader_sets,
-                            tx,
-                            shutdown_rx,
-                            num_threads,
-                            |reader, record_set| Ok(record_set.fill(reader)?),
-                        )
-                    });
+    fn new_record_set(&self) -> Self::RecordSet;
+    fn fill(&mut self, record: &mut Self::RecordSet) -> std::result::Result<bool, crate::Error>;
 
-                    // Spawn worker threads
-                    let mut handles = Vec::new();
-                    for thread_id in 0..num_threads {
-                        let worker_sets = Arc::clone(&record_sets);
-                        let worker_rx = rx.clone();
-                        let worker_shutdown_tx = shutdown_tx.clone();
-                        let worker_processor = processor.clone();
-
-                        let handle = scope.spawn(move || {
-                            run_paired_worker_thread(
-                                worker_sets,
-                                worker_rx,
-                                worker_shutdown_tx,
-                                worker_processor,
-                                thread_id,
-                                |record_set_pair, processor| {
-                                    let mut records1 = record_set_pair.0.iter();
-                                    let mut records2 = record_set_pair.1.iter();
-
-                                    // Process records in pairs
-                                    loop {
-                                        match (records1.next(), records2.next()) {
-                                            (Some(r1), Some(r2)) => {
-                                                processor.process_record_pair(r1?, r2?)?;
-                                            }
-                                            (Some(_), None) => {
-                                                // Record count mismatch between files // R2 has less records
-                                                return Err(ProcessError::PairedRecordMismatch(
-                                                    RecordPair::R2,
-                                                ));
-                                            }
-                                            (None, Some(_)) => {
-                                                // Record count mismatch between files // R1 has less records
-                                                return Err(ProcessError::PairedRecordMismatch(
-                                                    RecordPair::R1,
-                                                ));
-                                            }
-                                            (None, None) => break,
-                                        }
-                                    }
-                                    Ok(())
-                                },
-                            )
-                        });
-
-                        handles.push(handle);
-                    }
-
-                    // Wait for reader thread
-                    match reader_handle.join() {
-                        Ok(Ok(())) => (),
-                        Ok(Err(e)) => return Err(e),
-                        Err(_) => return Err(ProcessError::JoinError),
-                    }
-
-                    // Wait for worker threads
-                    for handle in handles {
-                        match handle.join() {
-                            Ok(Ok(())) => (),
-                            Ok(Err(e)) => return Err(e),
-                            Err(_) => return Err(ProcessError::JoinError),
-                        }
-                    }
-
-                    Ok(())
-                })?;
-
-                Ok(())
-            }
-
-            fn process_sequential_paired<T>(self, reader2: Self, mut processor: T) -> Result<()>
-            where
-                T: PairedParallelProcessor,
-            {
-                let mut reader1 = self;
-                let mut reader2 = reader2;
-                let mut record_set1 = <$record_set>::default();
-                let mut record_set2 = <$record_set>::default();
-
-                loop {
-                    match (
-                        record_set1.fill(&mut reader1)?,
-                        record_set2.fill(&mut reader2)?,
-                    ) {
-                        (true, true) => {
-                            for (record1, record2) in record_set1.iter().zip(record_set2.iter()) {
-                                let record1 = record1?;
-                                let record2 = record2?;
-                                processor.process_record_pair(record1, record2)?;
-                            }
-                            processor.on_batch_complete()?;
-                        }
-                        (true, false) => {
-                            // Record count mismatch between files // R2 has less records
-                            return Err(ProcessError::PairedRecordMismatch(RecordPair::R2));
-                        }
-                        (false, true) => {
-                            // Record count mismatch between files // R1 has less records
-                            return Err(ProcessError::PairedRecordMismatch(RecordPair::R1));
-                        }
-                        (false, false) => break,
-                    }
-                }
-                processor.on_thread_complete()?;
-                Ok(())
-            }
-        }
-    };
+    fn iter(
+        record_set: &Self::RecordSet,
+    ) -> impl Iterator<Item = std::result::Result<Self::RefRecord<'_>, crate::Error>>;
 }
 
-// Use the macro to implement for both FASTA and FASTQ
-impl_paired_parallel_reader!(
-    crate::fasta::Reader<R>,
-    crate::fasta::RecordSet,
-    crate::fasta::Error
-);
-impl_paired_parallel_reader!(
-    crate::fastq::Reader<R>,
-    crate::fastq::RecordSet,
-    crate::fastq::Error
-);
+impl<R> PairedParallelReaderSupport for crate::fasta::Reader<R>
+where
+    R: io::Read + Send,
+{
+    type RecordSet = crate::fasta::RecordSet;
+    type Error = crate::Error;
+    type RefRecord<'a> = crate::fasta::RefRecord<'a>;
+
+    fn new_record_set(&self) -> Self::RecordSet {
+        if let Some(batch_size) = self.batch_size {
+            Self::RecordSet::new(batch_size)
+        } else {
+            Self::RecordSet::default()
+        }
+    }
+
+    fn fill(&mut self, record: &mut Self::RecordSet ) -> std::result::Result<bool, crate::Error>{
+        record.fill(self)
+    }
+
+    fn iter(record_set: & Self::RecordSet) -> impl Iterator<Item = std::result::Result<Self::RefRecord<'_>, crate::Error>> {
+        record_set.positions
+            .iter()
+            .map(move |&pos| Self::RefRecord::new(&record_set.buffer, pos))
+    }
+}
+
+impl<R> PairedParallelReaderSupport for crate::fastq::Reader<R>
+where
+    R: io::Read + Send,
+{
+    type RecordSet = crate::fastq::RecordSet;
+    type Error = crate::Error;
+    type RefRecord<'a> = crate::fastq::RefRecord<'a>;
+
+    fn new_record_set(&self) -> Self::RecordSet {
+        if let Some(batch_size) = self.batch_size {
+            Self::RecordSet::new(batch_size)
+        } else {
+            Self::RecordSet::default()
+        }
+    }
+
+    fn fill(&mut self, record: &mut Self::RecordSet ) -> std::result::Result<bool, Self::Error>{
+        record.fill(self)
+    }
+
+    fn iter(record_set: & Self::RecordSet) -> impl Iterator<Item = std::result::Result<Self::RefRecord<'_>, crate::Error>> {
+        record_set.positions
+            .iter()
+            .map(move |&pos| Self::RefRecord::new(&record_set.buffer, pos))
+    }
+}
+
+impl<S: PairedParallelReaderSupport, R> PairedParallelReader<R> for S
+where
+    S: ParallelReader<R>,
+    R: io::Read + Send,
+{
+    fn process_parallel_paired<T>(
+        self,
+        reader2: Self,
+        processor: T,
+        num_threads: usize,
+    ) -> Result<()>
+    where
+        T: PairedParallelProcessor,
+    {
+        if num_threads == 0 {
+            return Err(ProcessError::InvalidThreadCount);
+        }
+        if num_threads == 1 {
+            return self.process_sequential_paired(reader2, processor);
+        }
+        let record_sets = Arc::new(
+            (0..num_threads * 2)
+                .map(|_| Mutex::new((self.new_record_set(), reader2.new_record_set())))
+                .collect::<Vec<_>>(),
+        );
+        let (tx, rx) = create_channels(num_threads * 2);
+        let (shutdown_tx, shutdown_rx) = create_shutdown_channel();
+
+        thread::scope(|scope| -> Result<()> {
+            // Spawn reader thread
+            let reader_sets = Arc::clone(&record_sets);
+            let reader_handle = scope.spawn(move || -> Result<()> {
+                run_paired_reader_thread(
+                    self,
+                    reader2,
+                    reader_sets,
+                    tx,
+                    shutdown_rx,
+                    num_threads,
+                    |reader, record_set| Ok(reader.fill(record_set)?),
+                )
+            });
+
+            // Spawn worker threads
+            let mut handles = Vec::new();
+            for thread_id in 0..num_threads {
+                let worker_sets = Arc::clone(&record_sets);
+                let worker_rx = rx.clone();
+                let worker_shutdown_tx = shutdown_tx.clone();
+                let worker_processor = processor.clone();
+
+                let handle = scope.spawn(move || {
+                    run_paired_worker_thread(
+                        worker_sets,
+                        worker_rx,
+                        worker_shutdown_tx,
+                        worker_processor,
+                        thread_id,
+                        |record_set_pair, processor| {
+                            let mut records1 = Self::iter(&record_set_pair.0);
+                            let mut records2 = Self::iter(&record_set_pair.1);
+
+                            // Process records in pairs
+                            loop {
+                                match (records1.next(), records2.next()) {
+                                    (Some(r1), Some(r2)) => {
+                                        processor.process_record_pair(r1?, r2?)?;
+                                    }
+                                    (Some(_), None) => {
+                                        // Record count mismatch between files // R2 has less records
+                                        return Err(ProcessError::PairedRecordMismatch(
+                                            RecordPair::R2,
+                                        ));
+                                    }
+                                    (None, Some(_)) => {
+                                        // Record count mismatch between files // R1 has less records
+                                        return Err(ProcessError::PairedRecordMismatch(
+                                            RecordPair::R1,
+                                        ));
+                                    }
+                                    (None, None) => break,
+                                }
+                            }
+                            Ok(())
+                        },
+                    )
+                });
+
+                handles.push(handle);
+            }
+
+            // Wait for reader thread
+            match reader_handle.join() {
+                Ok(Ok(())) => (),
+                Ok(Err(e)) => return Err(e),
+                Err(_) => return Err(ProcessError::JoinError),
+            }
+
+            // Wait for worker threads
+            for handle in handles {
+                match handle.join() {
+                    Ok(Ok(())) => (),
+                    Ok(Err(e)) => return Err(e),
+                    Err(_) => return Err(ProcessError::JoinError),
+                }
+            }
+
+            Ok(())
+        })?;
+
+        Ok(())
+    }
+
+    fn process_sequential_paired<T>(self, reader2: Self, mut processor: T) -> Result<()>
+    where
+        T: PairedParallelProcessor,
+    {
+        let mut reader1 = self;
+        let mut reader2 = reader2;
+        let mut record_set1 = S::RecordSet::default();
+        let mut record_set2 = S::RecordSet::default();
+
+        loop {
+            match (
+                reader1.fill(&mut record_set1)?,
+                reader2.fill(&mut record_set2)?,
+            ) {
+                (true, true) => {
+                    for (record1, record2) in std::iter::zip(Self::iter(&record_set1), Self::iter(&record_set2)) {
+                        let record1 = record1?;
+                        let record2 = record2?;
+                        processor.process_record_pair(record1, record2)?;
+                    }
+                    processor.on_batch_complete()?;
+                }
+                (true, false) => {
+                    // Record count mismatch between files // R2 has less records
+                    return Err(ProcessError::PairedRecordMismatch(RecordPair::R2));
+                }
+                (false, true) => {
+                    // Record count mismatch between files // R1 has less records
+                    return Err(ProcessError::PairedRecordMismatch(RecordPair::R1));
+                }
+                (false, false) => break,
+            }
+        }
+        processor.on_thread_complete()?;
+        Ok(())
+    }
+}
