9dbcd64ecd72f77e39d4e8547dd46b057c0eb219
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-09-04 13:09:33 -0700

    feat(prelude): add parallel traits into prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,4 +1,6 @@
 pub use crate::{
-    parallel::{ParallelProcessor, ParallelReader},
+    parallel::{
+        MultiParallelProcessor, PairedParallelProcessor, ParallelProcessor, ParallelReader,
+    },
     Record,
 };
